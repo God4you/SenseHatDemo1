72aeb3ed2afad813ab1c6037cd955e67ccc2e25a
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-14 09:47:42 +0100

    while true

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from sense_hat import SenseHat
 
 import time
+while True:
+    sense = SenseHat()
+    blue = (0,0,255)
+    yellow = (255,255,0)
 
-sense = SenseHat()
-blue = (0,0,255)
-yellow = (255,255,0)
-
-sense.show_message("Send Nudes!", text_colour =yellow, back_colour = blue)
+    sense.show_message("Send Nudes", text_colour =yellow, back_colour = blue)
