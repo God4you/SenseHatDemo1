ed82c9a1c89c769f51dc749fd0ac674d7a4c9a4d
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-14 09:43:07 +0100

    init main

---
+++ main.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+
+import time
+
+sense = SenseHat()
+blue = (0,0,255)
+yellow = (255,255,0)
+
+sense.show_message("Send Nudes!", text_colour =yellow, back_colour = blue)
