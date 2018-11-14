51bb1ed24be10089c40ad7ba74e141f1c1dfed12
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-14 11:36:30 +0100

    eyeFlip

--- setPixels.py
+++ setPixels.py
@@ -5,13 +5,13 @@ sense = SenseHat()
 # Define some colours
 g = (0, 255, 0) # Green
 b = (0, 0, 0) # Black
-
+o = (0, 0, 255) # Blue
 # Set up where each colour will display
 creeper_pixels = [
     g, g, g, g, g, g, g, g,
     g, g, g, g, g, g, g, g,
     g, b, b, g, g, b, b, g,
-    g, b, b, g, g, b, b, g,
+    g, b, o, g, g, b, o, g,
     g, g, g, b, b, g, g, g,
     g, g, b, b, b, b, g, g,
     g, g, b, b, b, b, g, g,
@@ -20,3 +20,7 @@ creeper_pixels = [
 
 # Display these colours on the LED matrix
 sense.set_pixels(creeper_pixels)
+
+while True:
+    sleep(1)
+    sense.flip_h()
