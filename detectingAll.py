ea36a081a744df55872ff98d58215bcffd0b318e
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-14 13:42:53 +0100

    colour

--- detectingAll.py
+++ detectingAll.py
@@ -1,6 +1,10 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
+# Define the colours red and green
+red = (255, 0, 0)
+green = (0, 255, 0)
+
 while True:
 
   # Take readings from all three sensors
@@ -17,5 +21,10 @@ while True:
   # str() converts the value to a string so it can be concatenated
   message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
   
+  if t > 18.3 and t < 26.7:
+    bg = green
+  else:
+    bg = red
+  
   # Display the scrolling message
-  sense.show_message(message, scroll_speed=0.05)
+  sense.show_message(message, scroll_speed=0.05, back_colour=bg)
