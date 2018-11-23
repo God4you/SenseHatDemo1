b2b3efef8c98510b94f99a74696e50a1893ca39e
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-23 11:33:16 +0100

    broadcast

--- pythonUDPBroadcaster.py
+++ pythonUDPBroadcaster.py
@@ -1,10 +1,31 @@
 from sense_hat import SenseHat
 from time import sleep
+import socket
 sense = SenseHat()
 sense.clear()
 
+def setup_udp_socket():
+    # Setup UDP socket for broadcasting
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    s.settimeout(0.2)
+
+    #socket.bind(("", 44444))
+    return s
+
+s = setup_udp_socket()
+
+
 while True:
     bg = (0,0,0)
-    temp = sense.get_temperature()
-    sense.show_message(str(temp), scroll_speed=0.05, back_colour=bg)
+    temp = str(sense.get_temperature())
+
+    # Broadcast byte encoded message to port 37020 via UDP Socket
+    # .encode() converts message to bytes for udp to broadcast
+    s.sendto(temp.encode(), ('<broadcast>', 37020))
+
+    sense.show_message(temp, scroll_speed=0.05, back_colour=bg)
     sleep(1)
