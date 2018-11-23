b7859f576bf70571c4da7bb5a42a077113d18a72
Benjamin Kakar <benj3918@edu.easj.dk>
2018-11-23 10:47:58 +0100

    read data og print

---
+++ pythonUDPBroadcaster.py
@@ -0,0 +1,11 @@
+from sense_hat import SenseHat
+from time import sleep
+sense = SenseHat()
+sense.clear()
+
+while True:
+    bg = (0,0,0)
+    temp = sense.get_temperature()
+    sense.show_message(temp, scroll_speed=0.05, back_colour=bg)
+
+sleep(2000)
